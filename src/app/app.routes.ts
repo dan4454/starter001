52981e6b6aec46cd06eed04751b7289f4966a25a
DanP <danpr987@gmail.com>
2024-09-22 00:44:44 -0400

    asdf

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import { Route1Component } from './route1/route1.component';
 
 export const routes: Routes = [
 
-    {path: 'route1', loadComponent: () => import('./route1/route1.component').then(m=> Route1Component)},
+    {path: 'route1', loadComponent: () => import('./route1/route1.component').then(m=> m.Route1Component)},
   
 
 ];
