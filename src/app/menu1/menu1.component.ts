76937ca0290e361ed261244e14b2d174bb795f3e
DanP <danpr987@gmail.com>
2024-11-02 08:04:56 -0400

    asfd

--- src/app/menu1/menu1.component.ts
+++ src/app/menu1/menu1.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgbDropdown, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-menu1',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgbModule],
   templateUrl: './menu1.component.html',
   styleUrl: './menu1.component.scss'
 })
